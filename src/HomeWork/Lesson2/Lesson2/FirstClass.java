e707d70a45df3cb088cab48a61d912b4c3001153
“Olga <slesarenko.h@gmail.com>
2022-10-20 22:50:19 +0300

    Homework Lesson 2

---
+++ src/HomeWork/Lesson2/Lesson2/FirstClass.java
@@ -0,0 +1,75 @@
+package HomeWork.Lesson2.Lesson2;
+
+public class FirstClass {
+
+//global primitive
+
+    static int intPrimGlobal = 100;
+    static double doublePrimGlobal;
+
+//string
+
+    String stringLinkGlobal;
+
+//global wrapper
+
+    Integer intLinkGlobal;
+
+    public static void main(String[] args) {
+
+//local primitive
+
+        int intPrimLocal = 255;
+        double doublePrimLocal = 2.63;
+        int aBool = 4;
+        int bBool = 25;
+        boolean c = aBool < bBool;
+
+//local link
+
+        String stringLinkLocal = "раз все переделывала, и снова ";
+        Integer intLinkLocal = 9685;
+
+//using getters and setters + print variables
+
+        SecondClass anyClass = new SecondClass();
+        anyClass.setByteVar((byte)100);
+        Byte byteVar = anyClass.getByteVar();
+        System.out.print(byteVar+" ");
+
+        SecondClass someClass = new SecondClass();
+        someClass.setStrVar("раз и еще ");
+        String someName = someClass.getStrVar();
+        System.out.print(someName);
+
+//just print variables
+
+        System.out.print(intPrimLocal+intPrimGlobal+" ");
+        System.out.println(stringLinkLocal+intPrimGlobal+", и без Идеи все равно беда, " + c + " , а иначе задание по " + doublePrimGlobal);
+
+//bringing to one type and print
+
+        String Summ = String.valueOf(intPrimLocal+(int)doublePrimLocal);
+        System.out.println("Сумма int intPrimLocal и приведенной к int doublePrimLocal, конвертированная в string, = " + Summ);
+
+        byte one = 1;
+        short two = 2568;
+        int three = (int)one + (int)two;
+        System.out.println("int = "+three);
+
+        float four = 25.3f;
+        double five = (double)four + 247.99;
+        System.out.println("double = "+five);
+
+        int six = 599;
+        byte seven = (byte)six;
+        System.out.println("byte = "+seven);
+
+        int eight = 245792323;
+        float nine = (float)eight;
+        System.out.println("float = "+nine);
+
+    }
+
+}
+
---
+++ src/HomeWork/Lesson2/Lesson2/SecondClass.java
@@ -0,0 +1,165 @@
+package HomeWork.Lesson2.Lesson2;
+
+public class SecondClass {
+
+//add global variables
+
+    private byte byteVar;
+    private short shortVar;
+    private int intVar;
+    private long longVar;
+    private float flVar;
+    private double doublVar;
+    private char charVar;
+    private boolean boolVar;
+
+//add global link types
+
+    private String strVar;
+    private Byte byteWrapp;
+    private Short shortWrapp;
+    private Integer intWrapp;
+    private Long longWrapp;
+    private Float floatWrapp;
+    private Double doubWrapp;
+    private Character charWrapp;
+    private Boolean boolWrapp;
+
+//add getters & setters
+
+    public byte getByteVar() {
+        return byteVar;
+    }
+
+    public void setByteVar(byte byteVar) {
+        this.byteVar = byteVar;
+    }
+
+    public short getShortVar() {
+        return shortVar;
+    }
+
+    public void setShortVar(short shortVar) {
+        this.shortVar = shortVar;
+    }
+
+    public int getIntVar() {
+        return intVar;
+    }
+
+    public void setIntVar(int intVar) {
+        this.intVar = intVar;
+    }
+
+    public long getLongVar() {
+        return longVar;
+    }
+
+    public void setLongVar(long longVar) {
+        this.longVar = longVar;
+    }
+
+    public float getFlVar() {
+        return flVar;
+    }
+
+    public void setFlVar(float flVar) {
+        this.flVar = flVar;
+    }
+
+    public double getDoublVar() {
+        return doublVar;
+    }
+
+    public void setDoubVar(double doublVar) {
+        this.doublVar = doublVar;
+    }
+
+    public char getCharVar() {
+        return charVar;
+    }
+
+    public void setCharVar(char charVar) {
+        this.charVar = charVar;
+    }
+
+    public boolean getBoolVar(boolean b) {
+        return boolVar;
+    }
+
+    public void setBoolVar(boolean boolVar) {
+        this.boolVar = boolVar;
+    }
+
+    public String getStrVar() {
+        return strVar;
+    }
+
+    public void setStrVar(String strVar) {
+        this.strVar = strVar;
+    }
+
+    public Byte getByteWrapp() {
+        return byteWrapp;
+    }
+
+    public void setByteWrapp(Byte byteWrapp) {
+        this.byteWrapp = byteWrapp;
+    }
+
+    public Short getShortWrapp() {
+        return shortWrapp;
+    }
+
+    public void setShortWrapp(Short shortWrapp) {
+        this.shortWrapp = shortWrapp;
+    }
+
+    public Integer getIntWrapp() {
+        return intWrapp;
+    }
+
+    public void setIntWrapp(Integer intWrapp) {
+        this.intWrapp = intWrapp;
+    }
+
+    public Long getLongWrapp() {
+        return longWrapp;
+    }
+
+    public void setLongWrapp(Long longWrapp) {
+        this.longWrapp = longWrapp;
+    }
+
+    public Float getFloatWrapp() {
+        return floatWrapp;
+    }
+
+    public void setFloatWrapp(Float floatWrapp) {
+        this.floatWrapp = floatWrapp;
+    }
+
+    public Double getDoubWrapp() {
+        return doubWrapp;
+    }
+
+    public void setDoubWrapp(Double doubWrapp) {
+        this.doubWrapp = doubWrapp;
+    }
+
+    public Character getCharWrapp() {
+        return charWrapp;
+    }
+
+    public void setCharWrapp(Character charWrapp) {
+        this.charWrapp = charWrapp;
+    }
+
+    public Boolean getBoolWrapp() {
+        return boolWrapp;
+    }
+
+    public void setBoolWrapp(Boolean boolWrapp) {
+        this.boolWrapp = boolWrapp;
+    }
+}
